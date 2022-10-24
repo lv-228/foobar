3813816369b6a0cdec94a57b1c24092b48b064df
lv-228 <massefick@gmail.com>
2022-10-24 06:58:43 +0300

    add php doc

---
+++ php_doc/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\representUrl\u0028\u0029",
+            "name": "representUrl",
+            "summary": "\u041F\u0440\u0435\u043E\u0431\u0440\u0430\u0437\u0443\u0435\u0442\u0020\u0438\u0441\u0445\u043E\u0434\u043D\u044B\u0439\u0020URL\u0020\u043F\u043E\u0020\u043D\u0435\u0441\u043A\u043E\u043B\u044C\u043A\u0438\u043C\u0020\u043A\u0440\u0438\u0442\u0435\u0440\u0438\u044F\u043C",
+            "url": "namespaces/default.html#function_representUrl"
+        },                {
+            "fqsen": "\\load_users_data\u0028\u0029",
+            "name": "load_users_data",
+            "summary": "",
+            "url": "namespaces/default.html#function_load_users_data"
+        },                {
+            "fqsen": "\\load_users_data_refactor\u0028\u0029",
+            "name": "load_users_data_refactor",
+            "summary": "\u041F\u043E\u043B\u0443\u0447\u0430\u0435\u0442\u0020\u0434\u0430\u043D\u043D\u044B\u0435\u0020\u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u0435\u0439\u0020\u043F\u043E\u0020\u0441\u0442\u0440\u043E\u043A\u0435\u0020\u0441\u043E\u0434\u0435\u0440\u0436\u0430\u0449\u0435\u0439\u0020id",
+            "url": "namespaces/default.html#function_load_users_data_refactor"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
